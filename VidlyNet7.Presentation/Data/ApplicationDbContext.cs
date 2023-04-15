70aadbe03a30560d9f61f5de3a82199fd9cc1586
MirMahmoudi <mirhabib.mahmoudi@gmail.com>
2023-04-15 14:34:22 +0200

    First Initialization

---
+++ VidlyNet7.Presentation/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace VidlyNet7.Presentation.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ VidlyNet7.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VidlyNet7.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

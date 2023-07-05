beec553e3fb2bbbd67b71ca68068b211cfca395a
MirMahmoudi <mirhabib.mahmoudi@gmail.com>
2023-07-05 14:24:44 +0200

    Models

---
+++ VidlyNet7.Presentation/Models/MembershipType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VidlyNet7.Presentation.Models
+{
+	public class MembershipType
+	{
+		public int Id { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+
+		public short SignUpFee { get; set; }
+		public byte DurationInMonth { get; set; }
+		public byte DiscountRate { get; set; }
+	}
+}

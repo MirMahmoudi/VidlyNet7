2605fa19e113ba87d96202bad94c70b9154e7335
MirMahmoudi <mirhabib.mahmoudi@gmail.com>
2023-07-17 13:59:29 +0200

    Dto

---
+++ VidlyNet7.Presentation/Dto/MapperConfig.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using VidlyNet7.Presentation.Models;
+
+namespace VidlyNet7.Presentation.Dto
+{
+	public class MapperConfig
+	{
+		public static Mapper InitializeAutoMapper()
+		{
+			var config = new MapperConfiguration(cfg =>
+			{
+				// Customer
+				cfg.CreateMap<Customer, CustomerDto>();
+				cfg.CreateMap<CustomerDto, Customer>()
+					.ForMember(c => c.Id, opt => opt.Ignore());
+
+				// Movie
+				cfg.CreateMap<Movie, MovieDto>();
+				cfg.CreateMap<MovieDto, Movie>()
+					.ForMember(c => c.Id, opt => opt.Ignore());
+			});
+
+			return new Mapper(config);
+		}
+	}
+}
---
+++ VidlyNet7.Presentation/Dto/MovieDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VidlyNet7.Presentation.Dto
+{
+	public class MovieDto
+	{
+		public int Id { get; set; }
+
+		[Required, StringLength(255)]
+		public string Name { get; set; } = string.Empty;
+
+		[Required]
+		public int GenreId { get; set; }
+
+		public DateTime? DateAdded { get; set; }
+
+		public DateTime ReleaseDate { get; set; }
+
+		[Range(1, 20)]
+		public int NumberInStock { get; set; }
+	}
+}

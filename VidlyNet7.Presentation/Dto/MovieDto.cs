e60eef48e456ce1c4a41e3a505deebb89c723225
MirMahmoudi <mirhabib.mahmoudi@gmail.com>
2023-07-19 16:02:37 +0200

    GenreDto & MembershipDto with their Mappings

--- VidlyNet7.Presentation/Dto/MovieDto.cs
+++ VidlyNet7.Presentation/Dto/MovieDto.cs
@@ -12,6 +12,8 @@ namespace VidlyNet7.Presentation.Dto
 		[Required]
 		public int GenreId { get; set; }
 
+		public GenreDto? Genre { get; set; }
+
 		public DateTime? DateAdded { get; set; }
 
 		public DateTime ReleaseDate { get; set; }
